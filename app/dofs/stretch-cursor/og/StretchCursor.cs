394ad51ae68d470acd9e32bfabc51817c42dc651
spatialfree <cadetmerchant@gmail.com>
2022-10-03 17:28:37 -0400

    final stretch-cursor pattern revision

--- app/dofs/stretch-cursor/og/StretchCursor.cs
+++ app/dofs/stretch-cursor/og/StretchCursor.cs
@@ -7,23 +7,20 @@ class StretchCursor : dof {
 	// data 
   public Vec3 cursor;
 
-  public void Init() {
-		cursor = to.position;
-	}
+  public void Init() {}
 
   public void Frame() {
-    Vec3   vec      = to.position - from.position;
-    float  len      = vec.Length;
-    float  stretch  = Math.Max(len - deadzone, 0f);
-    Vec3   dir      = pointer ? vec / len : to.orientation * Vec3.Forward;
+    Vec3  vec = to.position - from.position;
+    float len = vec.Length;
+    Vec3 	dir = vec / len;
 
+    float stretch = Math.Max(len - deadzone, 0f);
     cursor = to.position + dir * stretch * strength;
 
     Mesh.Cube.Draw(Material.Default, Matrix.TS(cursor, 0.01f));
   }
 
 	// design
-  public bool  pointer  = false;
   public float deadzone = 0.1f;
   public float strength = 3f;
 }
